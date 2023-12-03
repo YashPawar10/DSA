3afd3a6c1545c2b2cf001f2f10a6f5351f02522a
YashPawar10 <yashrpawar13@gmail.com>
2023-12-03 21:25:28 +0530

    leetcode code added

---
+++ array/121_Best_Time_To_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        int mxprofit=0;
+        int mi=arr[0];
+        for(int i=1;i<n;i++){
+            mxprofit=max(mxprofit,arr[i]-mi);
+            mi=min(mi,arr[i]);
+        }
+        return mxprofit;
+    }
+};
