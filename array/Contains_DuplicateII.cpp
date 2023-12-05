486ad3d28a11184783c09e5a46d1d1d075d3d467
YashPawar10 <yashrpawar13@gmail.com>
2023-12-05 19:30:54 +0530

    new code added

---
+++ array/Contains_DuplicateII.cpp
@@ -0,0 +1,22 @@
+// https : // leetcode.com/problems/contains-duplicate/description/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // unordered_map<int,int>m;
+        // for(int i=0;i<nums.size();i++){
+        //     m[nums[i]]++;
+        // }
+        // for(auto x:m){
+        //     if(x.second>1)return true;
+        // }
+        // return false;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1])return 1;
+        }
+        return false;
+    }
+};
---
+++ array/Single_Number.cpp
@@ -0,0 +1,13 @@
+// https://www.codingninjas.com/studio/problems/occurs-once_1214969?leftPanelTabValue=PROBLEM
+
+#include <bits/stdc++.h> 
+using namespace std;
+int occursOnce(vector<int> &a, int n){
+	// Write your code here.
+	int ans=0;
+	for(int i=0;i<n;i++){
+		ans^=a[i];
+	}
+	return ans;
+	
+}
